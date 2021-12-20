e93361cdfb0bfbb940ba547fa3b1ec438a10c1cc
Clara-Ro <lara.rhee.gree@gmail.com>
2021-12-20 11:33:55 -0300

    cenario adicionado

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as C from './App.styles'
+import {Character} from './components/Character'
+
+const App = () =>{
+  return(
+    <C.Container>
+      <C.Map>
+        <Character x={6} y={4} />
+      </C.Map>
+    </C.Container>
+  )
+}
+
+export default App
---
+++ src/hooks/useCharacter.ts
@@ -0,0 +1,5 @@
+import { useState } from "react"
+
+export const useCharacter = () => {
+    const[pos, setPos] = useState ({x:0, y:1})
+}
