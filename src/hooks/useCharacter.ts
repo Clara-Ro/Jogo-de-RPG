c5548d66a5cd5867a0477c1b5ef6e40bc1aa1ed5
Clara-Ro <lara.rhee.gree@gmail.com>
2021-12-20 15:46:31 -0300

    personagem feito

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,5 +1,48 @@
 import { useState } from "react"
+import { CharacterSides } from "../types/CharacterSides" 
 
 export const useCharacter = () => {
-    const[pos, setPos] = useState ({x:0, y:1})
+    const[pos, setPos] = useState ({ x: 3, y: 5 })
+    const[side, setSide] = useState <CharacterSides>('down')
+
+    const moveLeft = () => {
+        setPos(pos => ({
+            x:pos.x - 1,
+            y:pos.y
+        }))
+        setSide('left')
+    }
+
+    const moveRight = () => {
+        setPos(pos => ({
+            x:pos.x + 1,
+            y:pos.y
+        }))
+        setSide('right')
+    }
+
+    const moveUp = () => {
+        setPos(pos => ({
+            x:pos.x,
+            y:pos.y - 1
+        }))
+        setSide('up')
+    }
+
+    const moveDown = () => {
+        setPos(pos => ({
+            x:pos.x,
+            y:pos.y + 1
+        }))
+        setSide('down')
+    }
+    return{
+        x: pos.x,
+        y: pos.y,
+        side,
+        moveLeft,
+        moveDown,
+        moveRight,
+        moveUp
+    }
 }
